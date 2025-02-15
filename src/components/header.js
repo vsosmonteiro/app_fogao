91fb906b6f5cf0623b34d8787a53a13d871ff803
Victor Ferro <47899434+vsosmonteiro@users.noreply.github.com>
2025-02-14 23:12:42 -0300

    a

--- src/components/header.js
+++ src/components/header.js
@@ -1,46 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import minhaImagem from '../assets/logo.png';
 import './navbar.css';
 import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
-
   const navigate = useNavigate();
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <nav className="navbar">
       <div style={{ display: 'flex' }}>
-        <img src={minhaImagem} className='navbar-img' />
-        <div className="navbar-logo" >
-      
-        </div>
-
+        <img src={minhaImagem} className='navbar-img' alt="Logo" />
+        <div className="navbar-logo"></div>
       </div>
-      <button className="navbar-toggle">
       
-      </button>
-      <ul className={`navbar-menu`}>
-        <li>
-          <a href="/" onClick={(e)=>navigate('/')} >Home</a>
-        </li>
-       
-        <li>
-          <a href="/service" onClick={(e)=>navigate('/service')} >Services</a>
-        </li>
-    
-      </ul>
+      {!isMobile && ( // Remove buttons on mobile
+        <ul className="navbar-menu">
+          <li>
+            <a href="/" onClick={(e) => { e.preventDefault(); navigate('/'); }}>Home</a>
+          </li>
+          <li>
+            <a href="/service" onClick={(e) => { e.preventDefault(); navigate('/service'); }}>Services</a>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 };
-const styles = {
-  container: {
-    
-    height: "50",
-  
-    
-    
-  },
-};
-
 
 export default Navbar;
--- src/page/servicePage.js
+++ src/page/servicePage.js
@@ -1,95 +1,112 @@
 import { FaPhone, FaWhatsapp } from "react-icons/fa";
 import { useLocation } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
-function Service({}) {
+function Service({ }) {
+
+    const navigate = useNavigate();
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     const location = useLocation();
-    const { title, desc } = location.state || {}; 
+    const { title, desc } = location.state || {};
     return (
         <div style={{ minHeight: "100vh", color: "#545152", marginTop: 20 }}>
-            <div style={{ display: "flex", justifyContent:"space-between" }}>
-            <div>
-            <titlefog>
-                <div style={{ justifyItems: "left", marginLeft: 20 }}>
-                    <div style={style.desc1}>
-                        SERVIÇO
-                    </div>
-                    <div style={style.desc2}>
-                        {title??'Instalação de Fogão em João Pessoa'}
-                    </div>
-
-                </div>
-            </titlefog>
-            <div style={{...style.desc3 , width:"80%"}}>
-                    {'Oferecemos um serviço completo e especializado para instalação de fogão e projetos de rede de gás, atendendo às necessidades de prédios, condomínios, estabelecimentos comerciais, industriais e residências. Nossa equipe é composta por profissionais altamente capacitados, garantindo segurança, eficiência e conformidade com todas as normas técnicas.'}
-                    <div>
-                        <div style={style.title}>
-                        Quais os serviços que a Dr. Fogão oferece?
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <div>
+                    <titlefog>
+                        <div style={{ justifyItems: "left", marginLeft: 20 }}>
+                            <div style={style.desc1}>
+                                SERVIÇO
+                            </div>
+                            <div style={style.desc2}>
+                                {title ?? 'Instalação de Fogão em João Pessoa'}
+                            </div>
 
                         </div>
-                    
-<div>
-* Conserto de fogões residenciais: diagnóstico e reparo de falhas em queimadores, válvulas de gás, acendedores automáticos, termostatos e outros componentes.
-    </div>
-<div>* Instalação de fogões, cooktops e fornos: instalação precisa e alinhada com todas as normas de segurança, seja em gás GLP ou GN.
-    </div>
-<div>* Conversão de gás: realizamos a conversão de fogões de GLP (gás de botijão) para GN (gás natural) e vice-versa, garantindo um funcionamento adequado do aparelho.
-    </div>
-    <div>* 
-Realocação de ponto de gás: adaptação do ponto de gás para atender novas configurações na sua cozinha, com total segurança.
+                    </titlefog>
+                    <div style={{ ...style.desc3, width: "80%" }}>
+                        {'Oferecemos um serviço completo e especializado para instalação de fogão e projetos de rede de gás, atendendo às necessidades de prédios, condomínios, estabelecimentos comerciais, industriais e residências. Nossa equipe é composta por profissionais altamente capacitados, garantindo segurança, eficiência e conformidade com todas as normas técnicas.'}
+                        <div>
+                            <div style={style.title}>
+                                Quais os serviços que a Dr. Fogão oferece?
 
-    </div>
-<div style={style.title}>
-Por que escolher a Dr. Fogão?
-</div>
+                            </div>
 
-        <div>* Técnicos experientes e certificados</div>
-        <div>* Atendimento rápido e personalizado.</div>
-        <div>* Garantia de segurança e eficiência em todos os serviços.</div>
-        <div>* Suporte para marcas e modelos variados.</div>
-      
+                            <div>
+                                * Conserto de fogões residenciais: diagnóstico e reparo de falhas em queimadores, válvulas de gás, acendedores automáticos, termostatos e outros componentes.
+                            </div>
+                            <div>* Instalação de fogões, cooktops e fornos: instalação precisa e alinhada com todas as normas de segurança, seja em gás GLP ou GN.
+                            </div>
+                            <div>* Conversão de gás: realizamos a conversão de fogões de GLP (gás de botijão) para GN (gás natural) e vice-versa, garantindo um funcionamento adequado do aparelho.
+                            </div>
+                            <div>*
+                                Realocação de ponto de gás: adaptação do ponto de gás para atender novas configurações na sua cozinha, com total segurança.
 
-<div style={style.title}>
-Problemas com o fogão? A Dr. Fogão resolve! 
-</div>
+                            </div>
+                            <div style={style.title}>
+                                Por que escolher a Dr. Fogão?
+                            </div>
 
-<div style={style.title}>
-Somos especialistas em conserto de fogões em João Pessoa. Garantimos um serviço rápido, seguro e eficiente para deixar seu fogão como novo!
+                            <div>* Técnicos experientes e certificados</div>
+                            <div>* Atendimento rápido e personalizado.</div>
+                            <div>* Garantia de segurança e eficiência em todos os serviços.</div>
+                            <div>* Suporte para marcas e modelos variados.</div>
 
-Agende agora mesmo e conte com a expertise da Dr. Fogão. 
-</div>
 
+                            <div style={style.title}>
+                                Problemas com o fogão? A Dr. Fogão resolve!
+                            </div>
+
+                            <div style={style.title}>
+                                Somos especialistas em conserto de fogões em João Pessoa. Garantimos um serviço rápido, seguro e eficiente para deixar seu fogão como novo!
+
+                                Agende agora mesmo e conte com a expertise da Dr. Fogão.
+                            </div>
+
+                        </div>
                     </div>
-                </div>
-
-            </div>
-                <div style={{marginTop:0,marginRight:30 }}>
-                    <div style={style.styleRight}>
-                        Contato Dr. Fogão
-                    </div>
-                    <div style={style.button}>
-                        <FaWhatsapp size={40} ></FaWhatsapp>
-                    <div style={{marginLeft:20, textAlign:"left"}}>
-                    whatsapp
-                    <div>
-                        99999-9999
-                    </div>
-                    </div>
-                        
-                    </div>
-
-                    <div style={{...style.button,marginTop:20,color:"#F56A06"}}>
-                        <FaPhone size={40} ></FaPhone>
-                    <div style={{marginLeft:20, textAlign:"left"}}>
-                    Telefone
-                    <div>
-                        99999-9999
-                    </div>
-                    </div>
-                        
-                    </div>
-
 
                 </div>
+                
+                {!isMobile && (
+  <div style={{ marginTop: 0, marginRight: 30 }}>
+    <div style={style.styleRight}>
+      Contato Dr. Fogão
+    </div>
+    <div style={style.button}>
+      <FaWhatsapp size={40} />
+      <div style={{ marginLeft: 20, textAlign: "left" }}>
+        whatsapp
+        <div>
+          99999-9999
+        </div>
+      </div>
+    </div>
+
+    <div style={{ ...style.button, marginTop: 20, color: "#F56A06" }}>
+      <FaPhone size={40} />
+      <div style={{ marginLeft: 20, textAlign: "left" }}>
+        Telefone
+        <div>
+          99999-9999
+        </div>
+      </div>
+    </div>
+  </div>
+)}
+
 
             </div>
         </div>
@@ -116,30 +133,30 @@ const style = {
         textAlign: 'left',
         marginLeft: 20,
     },
-    styleRight:{
-        color:"#F56A06",
-        fontSize:32,
-        marginBottom:10,
-        fontWeight:600
+    styleRight: {
+        color: "#F56A06",
+        fontSize: 32,
+        marginBottom: 10,
+        fontWeight: 600
     },
-    button:{
+    button: {
         border: "2px solid #C7C7C7",
-        color:"#32A664",
-        justifyItems:"center",
-        alignItems  :"center",
-        fontSize:20,
-        fontWeight:600,
-        padding:10,
-        paddingLeft:30,
-        display:"flex",
-        width:300,
-        borderRadius:20
+        color: "#32A664",
+        justifyItems: "center",
+        alignItems: "center",
+        fontSize: 20,
+        fontWeight: 600,
+        padding: 10,
+        paddingLeft: 30,
+        display: "flex",
+        width: 300,
+        borderRadius: 20
     },
-    title:{
-        marginTop:20,
-        marginBottom:20,
-        fontSize:20,
-        fontWeight:700
+    title: {
+        marginTop: 20,
+        marginBottom: 20,
+        fontSize: 20,
+        fontWeight: 700
     }
 }
 
